53f3323a6acf041d97853472c9a7750c012aa57d
anas0522 <anas-2006-@hotmail.com>
2022-12-03 14:14:33 +0200

    sd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import Layout from "./components/layout/Layout";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes"
+import NewQuote from "./pages/NewQuote"
+import QuoteDetails from './pages/QuoteDetails'
+
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AllQuotes />} path="allQuotes" />
+          <Route element={<NewQuote />} path="addQuotes" />
+          <Route element={<QuoteDetails />} path="detailQuotes" />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
+  );
 }
 
 export default App;
